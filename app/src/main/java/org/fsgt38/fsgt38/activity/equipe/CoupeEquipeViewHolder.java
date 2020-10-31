0d1404d23ebb02d3f681aa290680023b7e7c61ee
psycholive <psycholive@psycholive.com>
2020-10-31 11:17:48 +0100

    Correction affichage des matches pour les coupes d'une équipe

--- app/src/main/java/org/fsgt38/fsgt38/activity/equipe/CoupeEquipeViewHolder.java
+++ app/src/main/java/org/fsgt38/fsgt38/activity/equipe/CoupeEquipeViewHolder.java
@@ -12,6 +12,13 @@ import org.fsgt38.fsgt38.model.Match;
 import org.fsgt38.fsgt38.util.TableauViewHolder;
 import org.fsgt38.fsgt38.util.Utils;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+import lombok.AllArgsConstructor;
+
 public class CoupeEquipeViewHolder extends TableauViewHolder<Equipe, Journee> {
 
 	// ----------------------------------------------------------------------------------------
@@ -35,51 +42,61 @@ public class CoupeEquipeViewHolder extends TableauViewHolder<Equipe, Journee> {
 		init(journee.getChampionnat().getNom());
 
 		Compteur compteur = new Compteur();
-		for (Match match: journee.getMatches())
-			affiche(equipe, match, 1, compteur);
+		Queue<MatchWrapper> matches = new ArrayDeque<>();
+		matches.add(new MatchWrapper(journee.getMatches()[0], 1));
+		affiche(equipe, matches, compteur);
 	}
 
 	/**
 	 * Affiche récursivement les matches
 	 * @param equipe Equipe
-	 * @param match Match
+	 * @param matches File des matches à traiter (pour le parcours de l'arbre par niveau)
 	 * @param compteur Index du prochain match (modifié dans la fonction)
 	 */
-	private void affiche(Equipe equipe, Match match, int niveau, Compteur compteur) {
+	private void affiche(Equipe equipe, Queue<MatchWrapper> matches, Compteur compteur) {
 
-		if (match == null) return;
+		MatchWrapper wrapper = matches.poll();
+		if (wrapper == null) return;
 
-		affiche(equipe, match.getMatch2(), niveau+1, compteur);
-		int i2 = compteur.i;
-		affiche(equipe, match.getMatch1(), niveau+1, compteur);
-		int i1 = compteur.i;
+		// On explore les enfants
+		if (wrapper.match.getMatch2() != null)
+			matches.add(new MatchWrapper(wrapper.match.getMatch2(), wrapper.niveau + 1));
+		if (wrapper.match.getMatch1() != null)
+			matches.add(new MatchWrapper(wrapper.match.getMatch1(), wrapper.niveau + 1));
+
+		affiche(equipe, matches, compteur);
+
+		Integer i1 = compteur.getIndex(wrapper.match.getMatch1());
+		Integer i2 = compteur.getIndex(wrapper.match.getMatch2());
 
 		// Ajout de la phase
-		if (niveau != compteur.lastNiveau) {
+		if (wrapper.niveau != compteur.lastNiveau) {
 			TableRow ligne = addLigneEntete();
 			ligne.setClickable(false);
 			if (compteur.i > 0)
 				((TableLayout.LayoutParams) ligne.getLayoutParams()).setMargins(0, Utils.convertDpToPixel(16, itemView.getContext()),0,0);
-			TextView cellule = addCelluleEntete(ligne, getTxtJournee(niveau));
+			TextView cellule = addCelluleEntete(ligne, getTxtJournee(wrapper.niveau));
 			((TableRow.LayoutParams) cellule.getLayoutParams()).span = 3;
-			compteur.lastNiveau = niveau;
+			compteur.lastNiveau = wrapper.niveau;
 			compteur.iStyle = 1;
 		}
 
 		compteur.i++;
 		compteur.iStyle++;
+		compteur.index.put(wrapper.match.getId(), compteur.i);
 
-		// Ajout du match
+		// Ajout du wrapper
 		int style = (compteur.iStyle % 2 == 0) ? R.layout.tableau_ligne_contenu_paire : R.layout.tableau_ligne_contenu_impaire;
-		if (match.getEquipe1() != null && match.getEquipe1().getId() == equipe.getId() ||
-			match.getEquipe2() != null && match.getEquipe2().getId() == equipe.getId())
+		if (wrapper.match.getEquipe1() != null && wrapper.match.getEquipe1().getId() == equipe.getId() ||
+			wrapper.match.getEquipe2() != null && wrapper.match.getEquipe2().getId() == equipe.getId())
 			style = R.layout.tableau_ligne_contenu_selection;
 
 		TableRow ligne = addLigne(style);
 		ligne.setClickable(false);
 		addCellule(ligne, compteur.i);
-		addCelluleEquipe(ligne, equipe, match.getEquipe1(), match.getScore1(), match.getScore2(), match.isForfait1(), match.isForfait2(), i1);
-		addCelluleEquipe(ligne, equipe, match.getEquipe2(), match.getScore2(), match.getScore1(), match.isForfait2(), match.isForfait1(), i2);
+		addCelluleEquipe(ligne, equipe, wrapper.match.getEquipe1(), wrapper.match.getScore1(), wrapper.match.getScore2(), wrapper.match.isForfait1(), wrapper.match.isForfait2(), i1);
+		addCellule(ligne, getDispScore(wrapper.match));
+		addCelluleEquipe(ligne, equipe, wrapper.match.getEquipe2(), wrapper.match.getScore2(), wrapper.match.getScore1(), wrapper.match.isForfait2(), wrapper.match.isForfait1(), i2);
 	}
 
 	/**
@@ -93,15 +110,29 @@ public class CoupeEquipeViewHolder extends TableauViewHolder<Equipe, Journee> {
 
 
 	// ----------------------------------------------------------------------------------------
-	//    Classe interne
+	//    Classes internes
 	// ----------------------------------------------------------------------------------------
 
 	/**
 	 * Entier modifiable
 	 */
-	private class Compteur {
+	private static class Compteur {
 		int i = 0;
 		int lastNiveau = 0;
 		int iStyle = 0;
+		Map<Integer, Integer> index = new HashMap<>();
+
+		Integer getIndex(Match match) {
+			return match == null ? null : index.get(match.getId());
+		}
+	}
+
+	/**
+	 * Ajoute des infos à un match
+	 */
+	@AllArgsConstructor
+	private static class MatchWrapper {
+		Match match;
+		int niveau;
 	}
 }
